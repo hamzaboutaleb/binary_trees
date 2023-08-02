1bf03f37941e4201f017c934f9b0ee11e397eb9f
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-02 10:04:46 +0100

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,6 +16,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	node->n = value;
 	node->parent = NULL;
+	node->left = NULL;
+	node->right = NULL;
 	if (parent)
 		node->parent = parent;
 
