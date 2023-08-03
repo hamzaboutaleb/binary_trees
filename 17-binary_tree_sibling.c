a16c0e19dbc8b353a9ca2160643eb5c196cfd5c5
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-03 11:17:21 +0100

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,12 +7,11 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	
+
 	if (node->parent->left == node)
 		return (node->parent->right);
 	else
-		return (node->parent->right);
+		return (node->parent->left);
 }
